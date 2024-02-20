86beec635d6d291bbab355f771623188fb661d15
peepee406 <71884583+peepee406@users.noreply.github.com>
2024-02-20 18:28:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,134 +21,29 @@ const deleteKeys = async (pattern) => {
   }
 }
 
-app.get("/api/v1/books", async (req, res) => {
-  const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
-  let page = +req.query?.page;
 
-  if (!page || page <= 0) page = 1;
-
-  const skip = (page - 1) * + limit;
-
-  const query = {};
-
-  if (keyword) query.name = { $regex: keyword, $options: "i" };
-
-  const key = `Book::${JSON.stringify({query, page, limit, orderBy, sortBy})}`
-  let response = null
+    app.post('/api/template', async (req, res) => {
   try {
-    const cache = await redis.get(key)
-    if (cache) {
-      response = JSON.parse(cache)
-    } else {
-      const data = await BookModel.find(query)
-      .skip(skip)
-      .limit(limit)
-      .sort({ [orderBy]: sortBy });
-      const totalItems = await BookModel.countDocuments(query);
-
-      response = {
-        msg: "Ok",
-        data,
-        totalItems,
-        totalPages: Math.ceil(totalItems / limit),
-        limit: +limit,
-        currentPage: page,
-      }
-
-      redis.setex(key, 600, JSON.stringify(response))
-    }
-    
-    return res.status(200).json(response);
+    const { name, roll } = req.body;
+    const template = new Template({ name, roll });
+    await template.save();
+    res.status(201).json({ message: 'Template saved successfully' });
   } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.get("/api/v1/books/:id", async (req, res) => {
+app.get('/api/templates', async (req, res) => {
   try {
-    const data = await BookModel.findById(req.params.id);
-
-    if (data) {
-      return res.status(200).json({
-        msg: "Ok",
-        data,
-      });
-    }
-
-    return res.status(404).json({
-      msg: "Not Found",
-    });
+    const templates = await Template.find();
+    res.json(templates);
   } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
-
-app.post("/api/v1/books", async (req, res) => {
-  try {
-    const { name, author, price, description } = req.body;
-    const book = new BookModel({
-      name,
-      author,
-      price,
-      description,
-    });
-    const data = await book.save();
-    deleteKeys('Book')
-    return res.status(200).json({
-      msg: "Ok",
-      data,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
-
-app.put("/api/v1/books/:id", async (req, res) => {
-  try {
-    const { name, author, price, description } = req.body;
-    const { id } = req.params;
-
-    const data = await BookModel.findByIdAndUpdate(
-      id,
-      {
-        name,
-        author,
-        price,
-        description,
-      },
-      { new: true }
-    );
-    deleteKeys('Book')
-    return res.status(200).json({
-      msg: "Ok",
-      data,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
-
-app.delete("/api/v1/books/:id", async (req, res) => {
-  try {
-    await BookModel.findByIdAndDelete(req.params.id);
-    deleteKeys('Book')
-    return res.status(200).json({
-      msg: "Ok",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
+  
 
 const PORT = process.env.PORT;
 
